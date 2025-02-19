2705082bc4af5408fe21b6d95452636eb37c4547
Anton <lipan48@gmail.com>
2025-02-19 19:13:54 +0300

    feat: implement intersection and focus directives

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Страница с постами</h1>
-    <my-input v-model="searchQuery" placeholder="Найти пост..." />
+    <my-input v-model="searchQuery" v-focus placeholder="Найти пост..." />
     <div class="app__btns">
       <my-button @click="showDialog">Создать пост</my-button>
       <my-select v-model="selectedSort" :options="sortOptions" />
@@ -16,7 +16,7 @@
       v-if="!isPostsLoading"
     />
     <div v-else>Идёт загрузка</div>
-    <div class="observer" ref="observer"></div>
+    <div class="observer" v-intersection="loadMorelPosts"></div>
   </div>
 </template>
 
@@ -103,18 +103,6 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-
-    const options = {
-      rootMargin: '8px',
-      threshold: 1.0,
-    };
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPage) {
-        this.loadMorelPosts();
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
